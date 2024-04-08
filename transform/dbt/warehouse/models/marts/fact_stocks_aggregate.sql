fc780aa02b09a7659dca3d96a8fc82d69a2bd489
Justin Joonsoo Lee <joonsmoons@gmail.com>
2024-04-08 19:06:49 +0900

    Finishing touches to modeling

--- transform/dbt/warehouse/models/marts/fact_stocks_aggregate.sql
+++ transform/dbt/warehouse/models/marts/fact_stocks_aggregate.sql
@@ -12,9 +12,6 @@ with polygon_stocks_topic as (
         toDate(toDateTime64(s / 1000, 3, 'America/New_York')) as dt,
         row_number() OVER (PARTITION BY sym, s ORDER BY s) AS unique_rnk
     from {{ source('raw', 'polygon_stocks_topic') }}
-), max_date as (
-    select max(dt) AS max_dt
-    from polygon_stocks_topic
 )
 select {{ dbt_utils.generate_surrogate_key(['sym', 's']) }} as stocks_aggregate_key,
     ev as event_type,
@@ -32,10 +29,9 @@ select {{ dbt_utils.generate_surrogate_key(['sym', 's']) }} as stocks_aggregate_
     toDateTime64(s / 1000, 3, 'America/New_York') AS starting_timestamp,
     toDateTime64(e / 1000, 3, 'America/New_York') as ending_timestamp,
     sym,
-    s
+    s,
+    dt
 from polygon_stocks_topic
-cross join max_date
 where 1 = 1
 and ev = 'AM'
 and unique_rnk = 1
-and dt = (select max_dt from max_date)
